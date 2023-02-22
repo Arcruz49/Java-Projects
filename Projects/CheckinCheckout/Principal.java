269c9b05396b2d3a5570ada86ac3af71bfd76349
Arcruz49 <106107472+Arcruz49@users.noreply.github.com>
2023-02-22 19:15:43 -0300

    Create Principal.java

---
+++ Projects/CheckinCheckout/Principal.java
@@ -0,0 +1,52 @@
+package application;
+
+import model.entities.DomainException;
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args){
+		Scanner teclado = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Room number: ");
+			int number = teclado.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(teclado.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(teclado.next());
+			
+
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: "+ reservation);
+				
+			System.out.println();
+			System.out.println("Enter data to update reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(teclado.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(teclado.next());
+				
+				
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: "+ reservation);
+		}
+		
+		catch (ParseException e) {
+			System.out.println("Invalid date format");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		teclado.close();
+	}
+}
